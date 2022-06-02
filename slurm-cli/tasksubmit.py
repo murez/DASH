d177c4b70be6adcf65c07dd6b048641bba88481c
yining765 <zhangyn3@shanghaitech.edu.cn>
2022-06-02 22:42:47 +0800

    change node

--- slurm-cli/tasksubmit.py
+++ slurm-cli/tasksubmit.py
@@ -1,32 +1,57 @@
 import subprocess
 import time
+import os
 
 def job_profile(path: str, job_name: int):
+    if not os.path.exists('tmp'):
+        os.makedirs('tmp')
+
     with open(path, 'r') as f:
         contents = f.readlines()
 
     to_insert = 0
-    for line in contents:
-        if not line.startswith('#'):
+    for to_insert, line in enumerate(contents):
+        if not line.lstrip().startswith('#'):
             break;
-        else:
-            to_insert += 1
 
-    if not contents[to_insert].startswith('(timeout 1m nvidia-smi'):
+    if not contents[to_insert].lstrip().startswith('(timeout 1m nvidia-smi'):
         contents.insert(to_insert, '(timeout 1m nvidia-smi --query-gpu=timestamp,utilization.gpu,utilization.memory,memory.free,memory.used,temperature.gpu,power.draw --format=csv -lms 100 > tmp/{}.log) &\n'.format(job_name))
-    with open('tmp/{}.run'.format(job_name), 'x') as f:
+    with open('tmp/{}.profile.run'.format(job_name), 'w+') as f:
+        f.writelines(contents)
+
+    response = subprocess.getoutput("sbatch tmp/{}.profile.run".format(job_name));
+    job_id = response.split(' ')[-1].strip()
+    print("The Job to profile is",job_id)
+    print(response)
+    return job_id
+
+def change_node(path: str, job_name: int, new_node: str):
+    if not os.path.exists('tmp'):
+        os.makedirs('tmp')
+
+    with open(path, 'r') as f:
+        contents = f.readlines()
+
+    to_insert = 0
+    for to_insert, line in enumerate(contents):
+        if line.lstrip().startswith('#'):
+            if line.lstrip().startswith('#SBATCH --nodelist='):
+                to_insert -= 1
+                contents.remove(to_insert)
+                break;
+        else:
+            break;
+
+    contents.insert(to_insert, '#SBATCH --nodelist={}\n'.format(new_node))
+    with open('tmp/{}.run'.format(job_name), 'w+') as f:
         f.writelines(contents)
 
     response = subprocess.getoutput("sbatch tmp/{}.run".format(job_name));
-    job_id = str(response).split(' ')[-1].strip()
-    print("The Job to profile is",job_id)
-    print(str(response))
+    job_id = response.split(' ')[-1].strip()
+    print("The Job to run",job_id)
+    print(response)
     return job_id
 
-def GetTaskOutputByID(id: int):
-    ret = subprocess.check_output("squeue -j {}".format(id), shell=True);
-    with open("{}.out".format(id), 'r+') as f:
-        print(f.read())
-
 if __name__ =="__main__":
-    id = job_profile("/public/home/qinfr/DASH/test/test.sbatch", 1)
+    id_profile = job_profile("/public/home/qinfr/DASH/test/test.sbatch", 1)
+    id = change_node("/public/home/qinfr/DASH/test/test.sbatch", 1, 'ai_gpu18')
